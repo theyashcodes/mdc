880087943b4b4517780666d2304dd0a51fbb6311
theyashcodes <iamyash.200@gmail.com>
2025-08-06 17:30:33 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,9 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def add(a, b):
+...     return a + b
+... 
+... if __name__ == "__main__":
+...     x = int(input("Enter first number: "))
+...     y = int(input("Enter second number: "))
+...     result = add(x, y)
